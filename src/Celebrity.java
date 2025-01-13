85f269c0049aa42571e036420fda50ff2cada814
Ananya Goel <ananyagoel2008@icloud.com>
2025-01-13 14:34:38 -0800

    Celebrity class

--- src/Celebrity.java
+++ src/Celebrity.java
@@ -1,2 +1,21 @@
-public class Celebrity {
+public class Celebrity extends Person{
+    private String occupation;
+
+    public Celebrity(String firstName, String lastName, String phoneNumber, String occupation) {
+        super(firstName, lastName, phoneNumber);
+        this.occupation = occupation;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Occupation: " + occupation;
+    }
 }
