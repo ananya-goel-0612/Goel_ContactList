1579c024c9b86cea2cb99cffa2e82011757ae3c8
Ananya Goel <ananyagoel2008@icloud.com>
2025-01-13 14:32:13 -0800

    Student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,21 @@
-public class Student {
+public class Student extends Person {
+    private int grade;
+
+    public Student(String firstName, String lastName, String phoneNumber, int grade) {
+        super(firstName, lastName, phoneNumber);
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Grade: " + grade;
+    }
 }
