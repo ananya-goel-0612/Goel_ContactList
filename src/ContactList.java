28df9038dae69fef1296b812cddcf88be6e3411e
Ananya Goel <ananyagoel2008@icloud.com>
2025-01-13 14:41:18 -0800

    added more todos

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class ContactList {
+    private ArrayList<Person> contacts;
+
+    public ContactList() {
+        contacts = new ArrayList<Person>();
+    }
+
+    public ArrayList<Person> getContacts() {
+        return contacts;
+    }
+
+    public void addContact() {
+       // TODO
+    }
+
+    public void printContacts() {
+        // TODO
+    }
+
+
 }
